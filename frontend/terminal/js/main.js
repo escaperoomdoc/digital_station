02afa8ee9e2f26e11b3e41c23ac76a0e40ca82e5
Alex Chiga <chiga@protonmail.com>
2019-04-05 13:49:19 +0300

    Добавлены базовые файлы проекта

---
+++ frontend/terminal/js/main.js
@@ -0,0 +1,411 @@
+const defaultRDM =  {
+    "diagram": {
+        "flow": {
+            "start": {
+                "state": "idle"
+            },
+            "engine_ready": {
+                "state": "idle"
+            },
+            "route_in": {
+                "state": "idle"
+            },
+            "arrival": {
+                "state": "idle"
+            },
+            "documents": {
+                "state": "idle"
+            },
+            "magistral": {
+                "state": "idle"
+            },
+            "pressure_head": {
+                "state": "idle"
+            },
+            "pressure_tail": {
+                "state": "idle"
+            },
+            "brakes": {
+                "state": "idle"
+            },
+            "brakes_head": {
+                "state": "idle"
+            },
+            "brakes_tail": {
+                "state": "idle"
+            },
+            "brakes_release": {
+                "state": "idle"
+            },
+            "release_head": {
+                "state": "idle"
+            },
+            "release_tail": {
+                "state": "idle"
+            },
+            "vu45": {
+                "state": "idle"
+            },
+            "unshoe": {
+                "state": "idle"
+            },
+            "depart_ready": {
+                "state": "idle"
+            },
+            "route_out": {
+                "state": "idle"
+            },
+            "departure": {
+                "state": "idle"
+            },
+            "accompaniment": {
+                "state": "idle"
+            },
+            "finish": {
+                "state": "idle"
+            }
+        }
+    },
+    "rdm":{
+        "ms_2n": {
+            "state": "free"
+        },
+        "ms_m3": {
+            "state": "free"
+        },
+        "ms_3a": {
+            "state": "free"
+        },
+        "ms_3_5": {
+            "state": "free"
+        },
+        "ms_5a": {
+            "state": "free"
+        },
+        "ms_2": {
+            "state": "free"
+        },
+        "ms_6p": {
+            "state": "free"
+        },
+        "ms_6_8": {
+            "state": "free"
+        },
+        "ms_8p": {
+            "state": "free"
+        },
+        "ms_m4": {
+            "state": "free"
+        },
+        "ms_2c": {
+            "state": "free"
+        },
+        "ms_1n": {
+            "state": "free"
+        },
+        "ms_m1": {
+            "state": "free"
+        },
+        "ms_7a": {
+            "state": "free"
+        },
+        "ms_7_9": {
+            "state": "free"
+        },
+        "ms_9p": {
+            "state": "free"
+        },
+        "ms_1": {
+            "state": "free"
+        },
+        "ms_2p": {
+            "state": "free"
+        },
+        "ms_2_4": {
+            "state": "free"
+        },
+        "ms_4p": {
+            "state": "free"
+        },
+        "ms_m2": {
+            "state": "free"
+        },
+        "ms_1c": {
+            "state": "free"
+        },
+        "ms_9m": {
+            "state": "free"
+        },
+        "ms_5": {
+            "state": "free"
+        },
+        "ms_2m": {
+            "state": "free"
+        },
+        "ms_7m": {
+            "state": "free"
+        },
+        "ms_5m": {
+            "state": "free"
+        },
+        "ms_4m": {
+            "state": "free"
+        },
+        "ms_6m": {
+            "state": "free"
+        },
+        "ms_3m": {
+            "state": "free"
+        },
+        "ms_1a": {
+            "state": "free"
+        },
+        "ms_1p": {
+            "state": "free"
+        },
+        "ms_3": {
+            "state": "free"
+        },
+        "ms_12p": {
+            "state": "free"
+        },
+        "ms_12a": {
+            "state": "free"
+        },
+        "ms_10p": {
+            "state": "free"
+        },
+        "ms_1m": {
+            "state": "free"
+        },
+        "ms_4": {
+            "state": "free"
+        },
+        "ms_12m": {
+            "state": "free"
+        },
+        "ms_m16": {
+            "state": "free"
+        },
+        "ms_10a": {
+            "state": "free"
+        },
+        "ms_10m": {
+            "state": "free"
+        },
+        "ms_8m": {
+            "state": "free"
+        },
+        "sw_1p": {
+            "state": "on"
+        },
+        "sw_1m": {
+            "state": "off"
+        },
+        "sw_2p": {
+            "state": "on"
+        },
+        "sw_2m": {
+            "state": "off"
+        },
+        "sw_3p": {
+            "state": "on"
+        },
+        "sw_3m": {
+            "state": "off"
+        },
+        "sw_4p": {
+            "state": "on"
+        },
+        "sw_4m": {
+            "state": "off"
+        },
+        "sw_5p": {
+            "state": "on"
+        },
+        "sw_5m": {
+            "state": "off"
+        },
+        "sw_6p": {
+            "state": "on"
+        },
+        "sw_6m": {
+            "state": "off"
+        },
+        "sw_7p": {
+            "state": "on"
+        },
+        "sw_7m": {
+            "state": "off"
+        },
+        "sw_8p": {
+            "state": "on"
+        },
+        "sw_8m": {
+            "state": "off"
+        },
+        "sw_9p": {
+            "state": "on"
+        },
+        "sw_9m": {
+            "state": "off"
+        },
+        "sw_10p": {
+            "state": "on"
+        },
+        "sw_10m": {
+            "state": "off"
+        },
+        "sw_12p": {
+            "state": "on"
+        },
+        "sw_12m": {
+            "state": "off"
+        },
+        "ls_1n": {
+            "state": "red"
+        },
+        "ls_2n": {
+            "state": "red"
+        },
+        "ls_n5": {
+            "state": "red"
+        },
+        "ls_n1": {
+            "state": "red"
+        },
+        "ls_n2": {
+            "state": "red"
+        },
+        "ls_n3": {
+            "state": "red"
+        },
+        "ls_n4": {
+            "state": "red"
+        },
+        "ls_2c": {
+            "state": "red"
+        },
+        "ls_1c": {
+            "state": "red"
+        },
+        "ls_c5": {
+            "state": "red"
+        },
+        "ls_c1": {
+            "state": "red"
+        },
+        "ls_c2": {
+            "state": "red"
+        },
+        "ls_c3": {
+            "state": "red"
+        },
+        "ls_c4": {
+            "state": "red"
+        },
+        "ls_m1": {
+            "state": "blue"
+        },
+        "ls_m3": {
+            "state": "blue"
+        },
+        "ls_m2": {
+            "state": "blue"
+        },
+        "ls_m4": {
+            "state": "blue"
+        },
+        "ls_m14": {
+            "state": "blue"
+        },
+        "ls_m16": {
+            "state": "blue"
+        },
+    }
+};
+var app = new Vue({
+    el: '.app',
+    data: {
+        event: JSON.parse(JSON.stringify(defaultRDM)),
+        time: "00:00:00"
+    },
+    methods: {
+        start() {
+            alert("start")
+        },
+        skip() {
+            counter++;
+            updateElement(str[counter]);
+        },
+        reset() {
+            this.event =  JSON.parse(JSON.stringify(defaultRDM));
+            counter = 0;
+            this.time = "00:00:00";
+        },
+    }
+})
+
+var counter = 0;
+var str;
+function read(){
+    var file = document.getElementById("file").files[0];
+    var reader = new FileReader();
+    reader.onload = function() {
+        str = reader.result.replace(/\r?\n/g, "");
+        str = JSON.parse(str);
+        counter = 0;
+        updateElement(str[counter]);
+        //app.event=JSON.parse(reader.result);
+        //document.getElementById("out").innerHTML=reader.result
+    };
+    reader.readAsText(file)
+}
+
+function updateModel(data) {
+    data.forEach(function(item) {
+        updateElement(item);
+    });
+}
+
+function updateElement(str) {
+    let time = str.time;
+    let type = str.type;
+    let object = str.object;
+    let param = str.param;
+    let value = str.value;
+
+    app.time = time;
+
+    switch (type) {
+        case "section": {
+            switch (param) {
+                case "state": {
+                    app.event.rdm[object].state = value;
+                    break;
+                }
+            }
+            break;
+        }
+        case "switch": {
+            switch (param) {
+                case "show": {
+                    app.event.rdm[object].state = value;
+                    break;
+                }
+            }
+            break;
+        }
+        case "light": {
+            switch (param) {
+                case "state": {
+                    app.event.rdm[object].state = value;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    //app.event.diagram.flow[object].state = value;
+}
