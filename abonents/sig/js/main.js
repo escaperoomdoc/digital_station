51ebdd5e8d2302971d13ae748ecae15d5d63eb08
Alex Chiga <chiga@protonmail.com>
2019-04-11 16:46:14 +0300

    updated abonent sig

--- abonents/sig/js/main.js
+++ abonents/sig/js/main.js
@@ -1,15 +1,26 @@
 var sig = new Vue({
     el: 'main',
     data: {
-        test: "test"
+        ways: [
+            { "way": "1П", "stock": "Технический осмотр", "status": "shunt", "active": false },
+            { "way": "2П", "stock": "Опробование тормозов", "status": "shunt", "active": false },
+            { "way": "3П", "stock": "Нет данных", "status": "free", "active": false },
+            { "way": "4П", "stock": "Нет данных", "status": "free", "active": false },
+            { "way": "5П", "stock": "4564 — Опробование тормозов", "status": "shunt", "active": true }
+        ],
+        selectedWay: 100
     },
     methods: {
         confirm() {
-            alert("ok");
             socket.emit('client2server', '{"command":"complete","getmodel":"true"}');
+            this.selectedWay = 100;
         },
         cancel() {
 
+        },
+        changeWay(i) {
+            if (this.selectedWay == i) this.selectedWay = 100;
+            else this.selectedWay = i;
         }
     }
 });
