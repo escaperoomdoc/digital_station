e91e8085f4560c79bf60669b326fed2825414990
Alex Chiga <chiga@protonmail.com>
2019-04-12 11:01:31 +0300

    added receive data to sig from server

--- abonents/sig/js/main.js
+++ abonents/sig/js/main.js
@@ -2,11 +2,11 @@ var sig = new Vue({
     el: 'main',
     data: {
         ways: [
-            { "way": "1П", "stock": "Технический осмотр", "status": "shunt", "active": false },
-            { "way": "2П", "stock": "Опробование тормозов", "status": "shunt", "active": false },
-            { "way": "3П", "stock": "Нет данных", "status": "free", "active": false },
-            { "way": "4П", "stock": "Нет данных", "status": "free", "active": false },
-            { "way": "5П", "stock": "4564 — Опробование тормозов", "status": "shunt", "active": true }
+            // { "way": "1П", "stock": "Технический осмотр", "status": "operation", "active": false },
+            // { "way": "2П", "stock": "Опробование тормозов", "status": "operation", "active": false },
+            // { "way": "3П", "stock": "Нет данных", "status": "free", "active": false },
+            // { "way": "4П", "stock": "Нет данных", "status": "free", "active": false },
+            // { "way": "5П", "stock": "4564 — Опробование тормозов", "status": "operation", "active": true }
         ],
         selectedWay: 100
     },
@@ -24,3 +24,25 @@ var sig = new Vue({
         }
     }
 });
+
+function updateSigList(obj) {
+    sig.ways.splice(0);
+    obj.abonents.forEach(function(item) {
+        let way    = obj.stocks.way;
+        let stock;
+        let status;
+
+        if (obj.stocks.status !== "") {
+            stock = obj.stocks.stock + " — " + obj.stocks.status;
+            status = "operation";
+        }
+        else {
+            stock = obj.stocks.stock;
+            status = "free";
+        }
+        let active = obj.stocks.active;
+
+        sig.ways.push({"way": way, "stock": stock, "status": status, "active": active});
+    });
+
+}
